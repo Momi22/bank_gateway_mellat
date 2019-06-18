74bd608ce4770c3449e8500f4a823b9ac406d32f
Mohammad <mohammad.shamami21@gmail.com>
2019-06-18 05:40:29 -0400

    main module for calling API

--- lib/mellat.rb
+++ lib/mellat.rb
@@ -1,6 +1,20 @@
 require "mellat/version"
+require_relative 'mellat/health_check'
+require_relative 'mellat/payment_request'
+require_relative 'mellat/payment_verification'
+require_relative 'mellat/configuration'
 
 module Mellat
-  class Error < StandardError; end
 
+  def self.up?
+    Mellat::ClientRequest.new.health_check
+  end
+
+  def self.authorize(params)
+    Mellat::PaymentRequest.new.apply(params)
+  end
+
+  def self.verify(params)
+    Mellat::PaymentRequest.new.verify(params)
+  end
 end
