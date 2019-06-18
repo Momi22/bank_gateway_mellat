41276264715682b82dc0aa9b5381dee9abc30007
Mohammad <mohammad.shamami21@gmail.com>
2019-06-18 04:55:58 -0400

    created cnfiguration file for specific attributes

--- lib/mellat/configuration.rb
+++ lib/mellat/configuration.rb
@@ -1,10 +1,25 @@
+require_relative './../initializer/mellat'
+
 module Mellat
 
-
-  class Configuration
-    attr_accessor :terminal_id, :username, :password, :payer_id, :retry_count,
-                  :authorize_wsdl
+  class << self
+    attr_accessor :configuration
   end
 
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield configuration
+  end
 
+  class Configuration
+    attr_accessor :terminal_id, :username, :password, :proxy, :retry_count, :web_service_wsdl
+
+    def retry_count
+      @retry_count || 3
+    end
+
+    def web_service_wsdl
+      @web_service_wsdl || 'https://bpm.shaparak.ir/pgwchannel/services/pgw?wsdl'
+    end
+  end
 end
